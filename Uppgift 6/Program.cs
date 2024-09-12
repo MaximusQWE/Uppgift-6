c04751c10c92a39e254fda7ba17493d68814bcba
mani06001 <mani06001@E-PW05C37P>
2024-09-12 12:58:04 +0200

    Add project files.

---
+++ Uppgift 6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift_1._4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("(&quot; /\\&quot;);");
+            Console.WriteLine("(&quot; / \\&quot;);");
+            Console.WriteLine("(&quot; / \&quot;\&quot; \\&quot;);");
+            Console.WriteLine("(&quot;/______\\&quot;);");
+        }
+    }
+}
